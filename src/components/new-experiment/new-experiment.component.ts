9cda546baddde91d41806574f9a74dadad0b83e7
mijim <migueljimenezbenajes@gmail.com>
2018-03-24 00:42:07 +0100

    Nuevos campos en formulario

--- src/components/new-experiment/new-experiment.component.ts
+++ src/components/new-experiment/new-experiment.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-experiment.component.css']
 })
 export class NewExperimentComponent implements OnInit {
-  
+  collapse: boolean;
   dateHourArray: Date[];
   dateHourSelected: Date;
   changingValueProgres: Number;
@@ -14,14 +14,24 @@ export class NewExperimentComponent implements OnInit {
   constructor() {
     this.dateHourArray = new Array<Date>();
     this.changingValueProgres = 100;
+    this.collapse = true;
   }
 
   ngOnInit() {
 
   }
   addDate(){
-    this.dateHourArray.push(this.dateHourSelected);
-    this.dateHourSelected = null
+    if(this.dateHourSelected != null){
+      this.dateHourArray.push(this.dateHourSelected);
+      this.dateHourSelected = null
+    }else{
+      //Alerta introducir hora
+    }
+    
+  }
+
+  deleteDate(i){
+    this.dateHourArray.splice(i,1)
   }
 
 }
