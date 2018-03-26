e02cb423185af2406da5c51a42327f2df4b0dd38
mijim <migueljimenezbenajes@gmail.com>
2018-03-26 15:08:06 +0200

    barra de progreso formulario nuevo experimento

--- src/components/new-experiment/new-experiment.component.ts
+++ src/components/new-experiment/new-experiment.component.ts
@@ -6,15 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-experiment.component.css']
 })
 export class NewExperimentComponent implements OnInit {
+  
   collapse: boolean;
+  changingValueProgres: number;
+  progresIncrement: number = 20;
+
   dateHourArray: Date[];
   dateHourSelected: Date;
-  changingValueProgres: Number;
+  dateHourEntered: boolean;
+  
+  title: String;
+  titleEntered: boolean;
+
+  place: String;
+  numberParticipants: number;
+  description: String;
 
   constructor() {
     this.dateHourArray = new Array<Date>();
-    this.changingValueProgres = 100;
+    this.changingValueProgres = 0;
     this.collapse = true;
+    this.titleEntered = false;
+    this.dateHourEntered = false;
   }
 
   ngOnInit() {
@@ -24,6 +37,10 @@ export class NewExperimentComponent implements OnInit {
     if(this.dateHourSelected != null){
       this.dateHourArray.push(this.dateHourSelected);
       this.dateHourSelected = null
+      if(!this.dateHourEntered){
+        this.dateHourEntered = true;
+        this.changingValueProgres += this.progresIncrement;
+      }
     }else{
       //Alerta introducir hora
     }
@@ -32,6 +49,23 @@ export class NewExperimentComponent implements OnInit {
 
   deleteDate(i){
     this.dateHourArray.splice(i,1)
+    if(this.dateHourArray.length == 0){
+      this.dateHourEntered = false;
+      this.changingValueProgres -= this.progresIncrement;
+    }
+  }
+
+  changeTitle(){
+    if(this.title == ""){
+      this.titleEntered = false;
+      this.changingValueProgres -= this.progresIncrement;
+      console.log(this.changingValueProgres);
+    }else{
+      if(this.titleEntered == false){
+        this.titleEntered = true;
+        this.changingValueProgres += this.progresIncrement;
+      }
+    }
   }
 
 }
