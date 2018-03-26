e92395abeb337517466cffa3d4f54b4550af5879
mijim <migueljimenezbenajes@gmail.com>
2018-03-26 15:37:36 +0200

    Progreso formulario

--- src/components/new-experiment/new-experiment.component.ts
+++ src/components/new-experiment/new-experiment.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FunctionsService} from '../../app/functions.service';
 
 @Component({
   selector: 'app-new-experiment',
@@ -7,32 +6,66 @@ import { FunctionsService} from '../../app/functions.service';
   styleUrls: ['./new-experiment.component.css']
 })
 export class NewExperimentComponent implements OnInit {
+  
   collapse: boolean;
+  changingValueProgres: number;
+  progresIncrement: number = 20;
+
   dateHourArray: Date[];
   dateHourSelected: Date;
-  changingValueProgres: Number;
+  dateHourEntered: boolean;
+  
+  title: String;
+  titleEntered: boolean;
 
-  constructor(public functions: FunctionsService) {
+  place: String;
+  numberParticipants: number;
+  description: String;
+
+  constructor() {
     this.dateHourArray = new Array<Date>();
-    this.changingValueProgres = 100;
+    this.changingValueProgres = 0;
     this.collapse = true;
+    this.titleEntered = false;
+    this.dateHourEntered = false;
   }
 
   ngOnInit() {
 
   }
-  addDate() {
-    if (this.dateHourSelected != null) {
+  addDate(){
+    if(this.dateHourSelected != null){
       this.dateHourArray.push(this.dateHourSelected);
-      this.dateHourSelected = null;
-    } else {
-      // Alerta introducir hora
+      this.dateHourSelected = null
+      if(!this.dateHourEntered){
+        this.dateHourEntered = true;
+        this.changingValueProgres += this.progresIncrement;
+      }
+    }else{
+      //Alerta introducir hora
     }
-
+    
   }
 
-  deleteDate(i) {
-    this.dateHourArray.splice(i, 1 );
+  deleteDate(i){
+    this.dateHourArray.splice(i,1)
+    if(this.dateHourArray.length == 0){
+      this.dateHourEntered = false;
+      this.changingValueProgres -= this.progresIncrement;
+    }
+  }
+
+  changeTitle(){
+    if(this.title == ""){
+      this.titleEntered = false;
+      this.changingValueProgres -= this.progresIncrement;
+      console.log(this.changingValueProgres);
+    }else{
+      if(this.titleEntered == false){
+        this.titleEntered = true;
+        this.changingValueProgres += this.progresIncrement;
+      }
+    }
   }
 
 }
