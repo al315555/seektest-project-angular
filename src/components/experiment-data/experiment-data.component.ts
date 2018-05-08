60ab9613c461e6a5a0a62cdac3c58cf81f9ad157
ruhimo <al315555@uji.es>
2018-05-08 17:36:16 +0200

    Vuelta atrás de valoraciones

--- src/components/experiment-data/experiment-data.component.ts
+++ src/components/experiment-data/experiment-data.component.ts
@@ -14,7 +14,6 @@ import {ModalConfirm} from '../modal/confirm-modal.component';
 import {ModalSize, SuiModalService} from 'ng2-semantic-ui';
 
 
-
 @Component({
   selector: 'app-experiment-data',
   templateUrl: './experiment-data.component.html',
@@ -29,11 +28,6 @@ export class ExperimentDataComponent implements OnInit {
   mes: string;
 
   isOwn: boolean;
-  experimentoRealizado: boolean;
-
-  numeroEstrellas: number;
-  numeroEstrellasMedias: number;
-  myVote: number;
 
   @Input() item: any;
   @Input() itemDates: Date[];
@@ -44,19 +38,16 @@ export class ExperimentDataComponent implements OnInit {
   sesionesInscrito: boolean[];
 
   estaInscrito: boolean;
-  today: Date;
 
   constructor(public experimentService: ExperimentsService , public functions: FunctionsService, private auth: AuthService,
               private modalService: SuiModalService) {
     this.monthDates = new Array();
     this.sesionesInscrito = new Array();
-    this.today = new Date();
-    this.myVote = -1;
   }
 
   ngOnInit() {
     this.itemDates.forEach(value => {
-      console.log('Valor:', value);
+      console.log(value);
       this.monthDates.push(value.getMonth());
     });
     if (this.item.uidPublisher === localStorage.getItem('uid_usuario')) {
@@ -64,47 +55,14 @@ export class ExperimentDataComponent implements OnInit {
     } else {
       this.isOwn = false;
     }
+
     if (this.item.inscriptions && this.item.inscriptions.length > 0) {
-      this.numeroEstrellas = 2;
-      this.experimentoRealizado = true;
-      this.item.inscriptions.forEach(element => {
-        if (element.uid === localStorage.getItem('uid_usuario')) {
-          const f = new Date();
-          f.setTime(element.session);
-          if (f <  new Date()) {
-            this.experimentoRealizado = false;
-          }
-        }
+      let indice = 0;
+      this.item.inscriptions.forEach(value => {
+        this.sesionesInscrito.push(value.uid === localStorage.getItem('uid_usuario'));
+        this.estaInscrito = value.uid === localStorage.getItem('uid_usuario');
+        indice++;
       });
-    }
-
-    this.myVote = this.experimentService.getMyVoteToExperiment(this.item);
-
-    console.log(this.myVote);
-
-    if (this.experimentoRealizado) {
-      if ((this.item.mediaValoracion !== 0) && (this.item.numberVotaciones !== 0)) {
-        this.numeroEstrellasMedias = Math.round(this.item.mediaValoracion / this.item.numberVotaciones);
-      } else {
-        this.numeroEstrellasMedias = 0;
-      }
-    }
-    /*if (this.item.date === ) {
-      this.isOwn = true;
-    } else {
-      this.isOwn = false;
-    }*/
-
-    if (this.item.inscriptions && this.item.inscriptions.length > 0) {
-        let indice = 0;
-        this.estaInscrito = false;
-        this.item.inscriptions.forEach(value => {
-          this.sesionesInscrito.push(value.uid === localStorage.getItem('uid_usuario'));
-          if (value.uid === localStorage.getItem('uid_usuario')) {
-            this.estaInscrito = value.uid === localStorage.getItem('uid_usuario');
-          }
-          indice++;
-        });
     }
     console.log(this.sesionesInscrito);
   }
@@ -148,14 +106,4 @@ export class ExperimentDataComponent implements OnInit {
     this.experimentService.updateInscriptionsOfExperiment(this.item);
   }
 
-  obtenerValor() {
-    this.experimentService.valorarExperimento(this.item, this.numeroEstrellas);
-    // console.log("Valoracion",this.numeroEstrellas)
-  }
-
-  yaHaPasado(date: Date): boolean {
-    return date > new Date();
-  }
 }
-
-
