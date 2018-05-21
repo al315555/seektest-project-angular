5986686c48bf9687892830c2c3273498bf993ef5
ruhimo <al315555@uji.es>
2018-05-21 17:56:38 +0200

    last commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyD4N93DU7pAGJLAeWPAF5ciFj9thx1FBQo",
-    authDomain: "seektest-3e130.firebaseapp.com",
-    databaseURL: "https://seektest-3e130.firebaseio.com",
-    projectId: "seektest-3e130",
-    storageBucket: "seektest-3e130.appspot.com",
-    messagingSenderId: "861021077426"
+    apiKey: 'AIzaSyD4N93DU7pAGJLAeWPAF5ciFj9thx1FBQo',
+    authDomain: 'seektest-3e130.firebaseapp.com',
+    databaseURL: 'https://seektest-3e130.firebaseio.com',
+    projectId: 'seektest-3e130',
+    storageBucket: 'seektest-3e130.appspot.com',
+    messagingSenderId: '861021077426'
   }
 };
